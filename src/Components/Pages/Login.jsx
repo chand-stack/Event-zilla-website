c2d4c4c068157fd9b970d5ec7020320e99b80b34
chand-stack <chand.stack@gmail.com>
2023-10-08 02:15:33 +0600

    Feat: firebase and auth provider added

--- src/Components/Pages/Login.jsx
+++ src/Components/Pages/Login.jsx
@@ -3,6 +3,8 @@ import { FcGoogle } from 'react-icons/fc';
 
 
 const Login = () => {
+
+
     return (
         <div>
             <div className=" bg-white  p-3 border-2 border-black my-5 mx-1 md:mx-auto lg:w-3/6 md:w-4/5">
