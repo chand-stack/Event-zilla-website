a39f1865643d7f7d6c8fccd9f7b4e30f0318cb56
chand-stack <chand.stack@gmail.com>
2023-10-08 03:34:22 +0600

    Feat: password validation and register function added

--- src/AuthProvider/AuthProvider.jsx
+++ src/AuthProvider/AuthProvider.jsx
@@ -1,12 +1,37 @@
-import { createContext } from "react";
+import { createUserWithEmailAndPassword, onAuthStateChanged, updateProfile } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import auth from "../firebase/firebase.config";
 
 
 export const AuthContext = createContext(null) 
 
 const AuthProvider = ({children}) => {
+    const[user,setUser] = useState(null)
+
+    const createUser = (email,password)=>{
+        return createUserWithEmailAndPassword(auth,email,password)
+    }
+
+    const updateUser = (name,photo) =>{
+        return updateProfile(auth.currentUser, {
+            displayName: `${name}`,photoURL:`${photo}` 
+        })
+    }
+
+    useEffect(()=>{
+        const unSubscribe = onAuthStateChanged(auth, currentUser => {
+            setUser(currentUser)
+            console.log(currentUser)
+        })
+        return () => {
+            unSubscribe()
+        }
+    },[])
 
     const authInfo = {
-
+          user,
+          createUser,
+          updateUser
     }
     return (
         <AuthContext.Provider value={authInfo}>
--- src/Components/Pages/Register.jsx
+++ src/Components/Pages/Register.jsx
@@ -1,8 +1,60 @@
 import { Link } from "react-router-dom";
 import { FcGoogle } from 'react-icons/fc';
+import { useContext } from "react";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+import swal from 'sweetalert';
 
 const Register = () => {
-    
+    const {createUser,updateUser} = useContext(AuthContext)
+
+    const registerHandler = e => {
+        e.preventDefault()
+        const name = e.target.name.value
+        const photo = e.target.photo.value
+        const email = e.target.email.value
+        const password = e.target.password.value
+
+
+        if(password.length<6){
+            swal({
+                icon: "error",
+                title:"Your password must be at least 6 characters"
+              });
+              return
+        }
+
+        if(!/(?=.*[!@#$%^&*])/.test(password)){
+            swal({
+                icon: "error",
+                title:"Password should contain atleast one special character"
+              });
+              return
+        }
+        if(!/(?=.*[A-Z])/.test(password)){
+            swal({
+                icon: "error",
+                title:"Password should contain atleast one capital letter"
+              });
+              return
+        }
+        
+
+        createUser(email,password)
+        .then(result => {
+            console.log(result.user)
+            updateUser(name,photo)
+            .then(()=>{})
+            .catch(()=>{})
+            swal({
+                icon: "success",
+                title:"Successfully Registered"
+              });
+        })
+        .catch(error => {
+            console.log(error)
+        })
+        
+    }
     return (
         <div>
             <div className=" bg-white p-3 border-2 border-black my-5 mx-1 md:mx-auto lg:w-3/6 md:w-4/5">
@@ -11,36 +63,48 @@ const Register = () => {
       <h1 className="text-5xl font-bold text-pink-500"><span className="text-black">Register</span> now!</h1>
     </div>
     <div className="card flex-shrink-0 w-full  bg-base-100">
-      <form className="card-body">
+      <form onSubmit={registerHandler} className="card-body">
         <div className="form-control">
           <label className="label">
             <span className="label-text font-bold">Name</span>
           </label>
-          <input type="text" placeholder="Your Name" className="input input-bordered" required />
+          <input type="text" 
+          placeholder="Your Name" 
+          name="name"
+          className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text font-bold">Photo URL</span>
           </label>
-          <input type="text" placeholder="Your Photo Url" className="input input-bordered" required />
+          <input type="text"
+           placeholder="Your Photo Url" 
+           name="photo"
+           className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text font-bold">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input type="email"
+           placeholder="email"
+           name="email"
+           className="input input-bordered" required />
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text font-bold">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input type="password" 
+          placeholder="password" 
+          name="password"
+          className="input input-bordered" required />
           <label className="label">
             <p className="label-text-alt link link-hover font-bold">Forgot password?</p>
           </label>
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary bg-black text-pink-500 font-bold border-none">Create an account</button>
+          <button type="submit" className="btn btn-primary bg-black text-pink-500 font-bold border-none">Create an account</button>
         </div>
         <p className="font-bold text-center">Already Have An Account? Please <Link to="/login" className="text-pink-500">Login</Link></p>
 
